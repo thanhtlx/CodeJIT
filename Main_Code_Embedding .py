16cb58ff4cc1f8be87f08a2ee7e2db395e159822
thanhtlx <thanh1908.xx@gmail.com>
2023-07-03 22:50:48 +0700

    tmp

--- Main_Code_Embedding .py
+++ Main_Code_Embedding .py
@@ -19,8 +19,15 @@ if __name__ == '__main__':
         os.makedirs(embedding_graph_dir)
     with open('map_ids_diff_cm.json') as f:
         mm = json.load(f)
+    total = len(mm)
+    cur = 0
+    per = 0
     for k,v in mm.items():
+        cur += 1 
         save_file = join(embedding_graph_dir,f'data_{k}.pt')
         _,data = model(torch.tensor(v,device=device)[None,:],return_dict=False)
         torch.save(data,save_file)
-    
+        tmp = cur/ total * 100
+        if tmp > per:
+            per = tmp
+            print(per)
